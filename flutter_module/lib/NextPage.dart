52c114438d0a671efeace106b779390f99369356
mengxinxin <mengxinxin@baidu.com>
2018-10-24 15:21:28 +0800

    移除无用文件；设置工程所有Target的 Enable Bitcode 为 NO

---
+++ flutter_module/lib/NextPage.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class NextPage extends StatefulWidget {
+  NextPage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _NextPageState createState() => new _NextPageState();
+}
+
+class _NextPageState extends State<NextPage> {
+  String _routeParams;
+
+  void gotoNextPage() {
+    Navigator.push(context, new MaterialPageRoute(builder: (BuildContext context){
+      return new NextPage(title: 'Flutter Next Page');
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return new Scaffold(
+      appBar: new AppBar(
+        // Here we take the value from the NextPage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: new Column(
+          // Column is also layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug paint" (press "p" in the console where you ran
+          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
+          // window in IntelliJ) to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new RaisedButton(
+                onPressed: gotoNextPage,
+                color: Colors.blue,
+                child: new Text('Goto Next Page',
+                    style: new TextStyle(color: Colors.white))),
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
